ba7850e5f29d84ba43d4d0842d5f107ac6e91a99
Ashish Saha <97810143+Ashish1100@users.noreply.github.com>
2023-01-20 22:09:24 +0530

    Update Introduction.cpp

--- Introduction.cpp
+++ Introduction.cpp
@@ -79,3 +79,111 @@ int main() {
 
     return 0;
 }
+
+
+
+//4. Array:
+
+#include<iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+       cin>>n;
+    int A[n];
+ 
+    for(int i=1;i<=n;i++)
+    {
+    cin>>A[i];
+    }
+    
+
+    for(int j=n;j>=1;j--)
+    {
+        cout<<A[j]<<" ";
+    }
+    return 0;
+    
+}
+
+
+
+
+//5. Dynamic Array:
+
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() 
+{
+    
+int x,y,s=0;
+cin>>x>>y;
+int *arr[x];
+while(x--)
+{
+
+int n;
+cin>>n;
+arr[s]=new int[n];
+for(int i=0;i<n;i++)
+{
+    cin>>arr[s][i];
+  
+
+}
+  s++;
+}
+while(y--)
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<arr[a][b]<<endl;
+    
+}
+
+
+  
+     
+    return 0;
+}
+
+
+int main()
+ {
+      
+    int n;
+    int q;
+    cin >> n >> q;
+    vector<int> a[n];
+    for(int i = 0; i < n; i++){
+        int m;
+        cin >> m;
+        int o;
+        for(int j = 0; j < m; j++){
+            cin >> o;
+            a[i].push_back(o);
+        }
+    }
+    
+    int r, s;
+    for(int k = 1; k <= q; k++){
+        cin >> r >> s;
+        cout << a[r][s] << endl;
+    }
+    return 0;
+}
+
